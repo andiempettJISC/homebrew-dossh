5c263a2e11c96ba50e1d059eba18f63d6c9940fd
andrew wilson <andrew.j.wilson7@gmail.com>
2023-02-07 10:43:50 +0000

    update version

--- dossh.rb
+++ dossh.rb
@@ -4,8 +4,8 @@
 class Dossh < Formula
   desc "A simple interactive CLI utility to lookup existing digitalocean droplets and initialise and SSH session"
   homepage "https://github.com/androidwiltron/dossh"
-  url "https://github.com/androidwiltron/dossh/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "03e670b059973bb6b1a1471bf8daea704d40d6c086086e53ca48ffef300a1285"
+  url "https://github.com/androidwiltron/dossh/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "499415039665eeaf775af9a8f3f6a465d051769f845546edf5278c53fe69b919"
   license "https://github.com/androidwiltron/dossh"
 
   depends_on "jq"
